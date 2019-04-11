d69d3eff3ab0acdf717bd4f335301d665c922f0e
Thomas Di Grégorio <thomas@devingfx.com>
2019-04-11 02:00:50 +0200

    Update RepeaterRandomPosition.js

--- generators/RepeaterRandomPosition.js
+++ generators/RepeaterRandomPosition.js
@@ -2,7 +2,7 @@ function init( event ) {
 
 	let { repeat, radius = 100 } = this.userData
 	this.templates = Array.from( this.children )
-	Array.from( this.children ).map( o=> o.remove() )
+	this.templates.map( o=> o.remove() )
 	
 	for( var i = 0; i < repeat || 10; i++ )
 	{
